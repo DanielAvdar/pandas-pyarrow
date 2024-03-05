037731143f589108c8a28601e702fb3c6403664e
DanielAvdar <66269169+DanielAvdar@users.noreply.github.com>
2024-03-05 20:50:13 +0200

    Refactor data type tests and update CI workflow
    Split data type tests into two distinct files for general and date-specific types, enhancing test organization. The refactoring moved general database data types testing to a new file called `test_db_general_dtypes.py`. Related changes are done in the continuous integration pipeline to accommodate the divide strategy.
    
    Signed-off-by: DanielAvdar <66269169+DanielAvdar@users.noreply.github.com>

--- tests/intg/test_db_general_dtypes.py
+++ tests/intg/test_db_general_dtypes.py
@@ -1,31 +1,7 @@
-import db_dtypes as dbdt
 import pandas as pd
 from parametrization import Parametrization
 
 from schemarrow.schema_arrow import SchemArrow
-
-
-@Parametrization.autodetect_parameters()
-@Parametrization.case(
-    name="dbdate case",
-    df_data=pd.DataFrame(
-        data={"col1": [pd.Timestamp("2021-01-01"), None]}, dtype=dbdt.DateDtype()
-    ),
-    expected_dtype="date32[pyarrow]",
-)
-@Parametrization.case(
-    name="dbtime case",
-    df_data=pd.DataFrame(
-        data={"col1": [pd.Timestamp("2021-01-01 12:00:00"), None]},
-        dtype=dbdt.TimeDtype(),
-    ),
-    expected_dtype="time64[us][pyarrow]",
-)
-def test_db_date_dtypes(df_data, expected_dtype):
-    sa = SchemArrow()
-    adf = sa(df_data)
-
-    assert adf.dtypes[0] == expected_dtype
 
 
 @Parametrization.autodetect_parameters()
