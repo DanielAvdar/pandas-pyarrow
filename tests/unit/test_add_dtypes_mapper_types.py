cd1b514fd05c82d070134a143f7fc369a29832d3
DanielAvdar <66269169+DanielAvdar@users.noreply.github.com>
2024-03-07 22:07:53 +0200

    Add new case for simple type mapping in unit tests
    Extended the "test_add_dtypes_types" function in unit tests by adding a new case for simple type mapping override with additional_mapper_dicts. This new test case will ensure the accuracy and efficiency of mapping "float64" to "float32[pyarrow]".
    
    Signed-off-by: DanielAvdar <66269169+DanielAvdar@users.noreply.github.com>

--- tests/unit/test_add_dtypes_mapper_types.py
+++ tests/unit/test_add_dtypes_mapper_types.py
@@ -23,6 +23,14 @@ from schemarrow.schema_arrow import SchemArrow
     expected_dtype="timestamp[ms,UTC][pyarrow]",
     additional_mapper_dicts={"datetime64[ms]": "timestamp[ms,UTC][pyarrow]"},
 )
+@Parametrization.case(
+    name="test simple type mapping override with additional_mapper_dicts ms",
+    df_data=pd.DataFrame(
+        {"test_column": [0.0000000001, 2.0, 3.0, None]}, dtype="float64"
+    ),
+    expected_dtype="float32[pyarrow]",
+    additional_mapper_dicts={"float64": "float32[pyarrow]"},
+)
 def test_add_dtypes_types(df_data, expected_dtype, additional_mapper_dicts):
     sa = SchemArrow(custom_mapper=additional_mapper_dicts)
     adf = sa(df_data)
