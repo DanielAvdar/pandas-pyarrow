07a2781e46348d8af85d91347fe6da490f37aa55
DanielAvdar <66269169+DanielAvdar@users.noreply.github.com>
2024-03-07 22:06:26 +0200

    Refactor property-based tests in pb_sts.py
    Adjusted the import statement to use 'composite' from 'hypothesis.strategies' directly. Also, simplified the function return types and organized the placements of 'dtype' parameters in the 'data_frames' function call for better readability. The refactoring did not change any functionality or behavior of the tests.
    
    Signed-off-by: DanielAvdar <66269169+DanielAvdar@users.noreply.github.com>

--- tests/unit/property_based/pb_sts.py
+++ tests/unit/property_based/pb_sts.py
@@ -3,6 +3,7 @@ from typing import Any, Dict, List, Tuple, Union
 import pandas as pd
 from hypothesis import strategies as st
 from hypothesis.extra.pandas import columns, data_frames, range_indexes
+from hypothesis.strategies import composite
 
 # Introduced constants
 DTYPES_SAMPLE: List[Union[type, str]] = [
@@ -22,8 +23,8 @@ DTYPES_SAMPLE: List[Union[type, str]] = [
 ]
 
 
-@st.composite
-def dtypes_st(draw: Any) -> st.SearchStrategy[Any]:
+@composite
+def dtypes_st(draw: Any) -> Any:
     dtypes = st.sampled_from(DTYPES_SAMPLE)
     return draw(dtypes)
 
@@ -32,40 +33,40 @@ def create_dataframe(draw: Any, gen_type: str, col_names: str) -> pd.DataFrame:
     dfs_st = data_frames(
         columns=columns(
             [col_names],
+            dtype=gen_type,
         ),
         index=range_indexes(
             min_size=1,
             max_size=6,
         ),
-        dtype=gen_type,
 
     )
     return draw(dfs_st)
 
 
-@st.composite
-def df_st(draw: Any) -> st.SearchStrategy[pd.DataFrame]:
+@composite
+def df_st(draw: Any) -> pd.DataFrame:
     col_names = draw(st.sets(st.text(min_size=1, max_size=10), min_size=2, max_size=5))
     dfs_st = data_frames(
         columns=columns(
             col_names,
+            dtype=draw(st.sampled_from(DTYPES_SAMPLE)),
         ),
         index=range_indexes(
             min_size=2,
             max_size=8,
         ),
-        dtype=draw(st.sampled_from(DTYPES_SAMPLE)),
 
     )
     return draw(dfs_st)
 
 
-@st.composite
+@composite
 def single_column_df_st(
         draw: Any,
         pair_mapping: Dict[str, str],
         gen_type: str,
-) -> st.SearchStrategy[Tuple[pd.DataFrame, str]]:
+) -> Tuple[pd.DataFrame, str]:
     col_name = draw(st.text(min_size=1, max_size=10))
     pair: Tuple[str, str] = draw(st.sampled_from(list(pair_mapping.items())))
     source_dtype_name, target_dtype_name = pair
