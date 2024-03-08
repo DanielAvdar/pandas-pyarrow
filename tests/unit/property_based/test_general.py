777df977e245d84687ae6117529831a2ee5d54d0
DanielAvdar <66269169+DanielAvdar@users.noreply.github.com>
2024-03-08 16:27:25 +0200

    Update general unit test in SchemArrow module
    The property-based general unit test for the SchemArrow module has been modified. A copy of the pandas DataFrame (df) is now created before it's passed to SchemArrow. Verification has been added to ensure the original df remains unmodified after passing through SchemArrow and every dtype in the resulting DataFrame is of type 'pyarrow'.
    
    Signed-off-by: DanielAvdar <66269169+DanielAvdar@users.noreply.github.com>

--- tests/unit/property_based/test_general.py
+++ tests/unit/property_based/test_general.py
@@ -7,8 +7,12 @@ from tests.unit.property_based.pb_sts import df_st
 @hp.given(df=df_st())
 @hp.settings(max_examples=500)
 def test_dtypes_hp(df):
+    df_copy = df.copy()
     sa = SchemArrow()
     adf = sa(df)
 
     new_dtypes_names = [repr(i) for i in adf.dtypes.tolist()]
-    assert all(["[pyarrow]" in dtype in new_dtypes_names for dtype in new_dtypes_names])
+    assert all(["[pyarrow]" in dtype in new_dtypes_names
+                for dtype in new_dtypes_names]), 'Some dtypes are not converted'
+    assert not df.equals(adf), 'The original df has been modified'
+    assert df.equals(df_copy), 'The original df has been modified'
