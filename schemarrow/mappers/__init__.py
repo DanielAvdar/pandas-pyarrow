30ad43bd61682a047e0aea87eb2709d3127653d5
DanielAvdar <66269169+DanielAvdar@users.noreply.github.com>
2024-03-09 14:12:04 +0200

    Refactor DateTimeMapper to a function.
    The previous DateTimeMapper class in the schemarrow library has been simplified and refactored into a standalone function, datetime_mapper. This change streamlines the code and improves readability and maintainability. Changes have also been reflected in the schemarrow library's __init__.py file to import the new function.
    
    Signed-off-by: DanielAvdar <66269169+DanielAvdar@users.noreply.github.com>

--- schemarrow/mappers/__init__.py
+++ schemarrow/mappers/__init__.py
@@ -1,6 +1,6 @@
 from typing import Dict
 
-from schemarrow.mappers.datetime_mapper import DateTimeMapper
+from schemarrow.mappers.datetime_mapper import datetime_mapper
 from schemarrow.mappers.db_types import mapper_db_types
 from schemarrow.mappers.dtype_mapper import mapper_dict_dt, mapper_dict_object
 from schemarrow.mappers.numeric_mapper import numeric_mapper
@@ -11,7 +11,7 @@ def create_mapper() -> Dict[str, str]:
         **numeric_mapper(["float"], ["16", "32", "64"]),
         **numeric_mapper(["int"], ["8", "16", "32", "64"]),
         **numeric_mapper(["Float", "Int"], ["32", "64"]),
-        **DateTimeMapper()(),
+        **datetime_mapper(),
         **mapper_dict_dt,
         **mapper_dict_object,
         **mapper_db_types,
@@ -24,6 +24,6 @@ __all__ = [
     "mapper_dict_object",
     "create_mapper",
     "mapper_db_types",
-    "DateTimeMapper",
+    "datetime_mapper",
     "numeric_mapper",
 ]
