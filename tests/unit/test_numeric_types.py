3dbd95713f99f8c8b52753c0ce40ad4f1c105203
DanielAvdar <66269169+DanielAvdar@users.noreply.github.com>
2024-03-11 11:56:54 +0200

    Refactor test case names for clarity
    The names of the test cases in the numeric types testing suite have been refactored for more clarity, following a consistent format of specifying the data type being tested. This makes it easier to understand which data types are being tested in each test case.
    
    Signed-off-by: DanielAvdar <66269169+DanielAvdar@users.noreply.github.com>

--- tests/unit/test_numeric_types.py
+++ tests/unit/test_numeric_types.py
@@ -6,7 +6,7 @@ from parametrization import Parametrization
 
 @Parametrization.autodetect_parameters()
 @Parametrization.case(
-    name="int64 case",
+    name="Case handling integer 64 bit data type",
     df_data=pd.DataFrame(
         {
             "test_column": [
@@ -20,7 +20,7 @@ from parametrization import Parametrization
     expected_dtype="int64[pyarrow]",
 )
 @Parametrization.case(
-    name="int32 case",
+    name="Case handling integer 32 bit data type",
     df_data=pd.DataFrame(
         {
             "test_column": [
@@ -34,22 +34,21 @@ from parametrization import Parametrization
     expected_dtype="int32[pyarrow]",
 )
 @Parametrization.case(
-    name="float64 case",
+    name="Case handling floating point 64 bit data type",
     df_data=pd.DataFrame({"test_column": [1.0, 2.0, 3.0, None]}, dtype="float64"),
     expected_dtype="float64[pyarrow]",
 )
 @Parametrization.case(
-    name="float32 case",
+    name="Case handling floating point 32 bit data type",
     df_data=pd.DataFrame({"test_column": [1.0, 2.0, 3.0, None]}, dtype="float32"),
     expected_dtype="float32[pyarrow]",
 )
 @Parametrization.case(
-    name="float32 case",
+    name="Case handling PyArrow specific floating point 32 bit data type",
     df_data=pd.DataFrame({"test_column": [1.0, 2.0, 3.0, None]}, dtype="float32[pyarrow]"),
     expected_dtype="float32[pyarrow]",
 )
 def test_numeric_types(df_data, expected_dtype):
     sa = PandasArrowConverter()
     adf = sa(df_data)
-
     assert list(adf.dtypes)[0] == expected_dtype
