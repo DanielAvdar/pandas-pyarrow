1043f50fb8669457c19337fb030ba763660a5b17
DanielAvdar <66269169+DanielAvdar@users.noreply.github.com>
2024-03-13 17:35:42 +0200

    Replace PandasArrowConverter with convert_to_pyarrow in tests
    The conversion process has been refactored for the tests in the pandas-pyarrow package. Instead of creating a PandasArrowConverter object for each test case, the convert_to_pyarrow function is directly used for converting the dataframes. This simplification makes the tests more straightforward and potentially more efficient.
    
    Signed-off-by: DanielAvdar <66269169+DanielAvdar@users.noreply.github.com>

--- tests/unit/property_based/test_numeric.py
+++ tests/unit/property_based/test_numeric.py
@@ -1,6 +1,6 @@
 from typing import Tuple
 
-from pandas_pyarrow import PandasArrowConverter
+from pandas_pyarrow import convert_to_pyarrow
 from pandas_pyarrow.mappers import numeric_mapper
 from tests.unit.property_based.pb_sts import single_column_df_st
 
@@ -19,9 +19,8 @@ import pandas as pd
     )
 )
 def test_float_numpy_api_hp(pair: Tuple[pd.DataFrame, str]):
-    sa = PandasArrowConverter()
     df, target_dtype = pair
-    adf = sa(df)
+    adf = convert_to_pyarrow(df)
 
     assert list(adf.dtypes)[0] == target_dtype
 
@@ -37,9 +36,8 @@ def test_float_numpy_api_hp(pair: Tuple[pd.DataFrame, str]):
     )
 )
 def test_float_array_api_hp(pair: Tuple[pd.DataFrame, str]):
-    sa = PandasArrowConverter()
     df, target_dtype = pair
-    adf = sa(df)
+    adf = convert_to_pyarrow(df)
 
     assert list(adf.dtypes)[0] == target_dtype
 
@@ -55,9 +53,8 @@ def test_float_array_api_hp(pair: Tuple[pd.DataFrame, str]):
     )
 )
 def test_int_numpy_api_hp(pair: Tuple[pd.DataFrame, str]):
-    sa = PandasArrowConverter()
     df, target_dtype = pair
-    adf = sa(df)
+    adf = convert_to_pyarrow(df)
 
     assert list(adf.dtypes)[0] == target_dtype
 
@@ -73,8 +70,7 @@ def test_int_numpy_api_hp(pair: Tuple[pd.DataFrame, str]):
     )
 )
 def test_int_array_api_hp(pair: Tuple[pd.DataFrame, str]):
-    sa = PandasArrowConverter()
     df, target_dtype = pair
-    adf = sa(df)
+    adf = convert_to_pyarrow(df)
 
     assert list(adf.dtypes)[0] == target_dtype
