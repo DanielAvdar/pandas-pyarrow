80951b3167d1fc699ec9f143102590820e63cf87
DanielAvdar <66269169+DanielAvdar@users.noreply.github.com>
2024-03-13 17:36:08 +0200

    Add new test cases in numeric types testing
    Three new test cases for numeric types i.e., float16, uint16 and complex64 have been added in the pandas-pyarrow package testing. These additions will ensure the tested coverage and compatibility of these data types within the pandas-pyarrow integration.
    
    Signed-off-by: DanielAvdar <66269169+DanielAvdar@users.noreply.github.com>

--- tests/unit/test_numeric_types.py
+++ tests/unit/test_numeric_types.py
@@ -21,6 +21,9 @@ def create_test_case(
 @create_test_case("int32", "int32[pyarrow]", [1, 2, 3])
 @create_test_case("float64", "float64[pyarrow]", [1.0, 2.0, 3.0, None])
 @create_test_case("float32", "float32[pyarrow]", [1.0, 2.0, 3.0, None])
+@create_test_case("float16", "float16[pyarrow]", [1.0, 2.0, 3.0, None])
+@create_test_case("uint16", "uint16[pyarrow]", [1, 2, 3])
+@create_test_case("complex64", "string[pyarrow]", [1, 2, 3])
 @create_test_case("float32[pyarrow]", "float32[pyarrow]", [1.0, 2.0, 3.0, None])
 def test_numeric_types(df_data, expected_dtype):
     sa = PandasArrowConverter()
