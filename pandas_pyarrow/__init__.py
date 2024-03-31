fa78a7e918f8479f68708d8bd4e73591fc2cd4b5
DanielAvdar <66269169+DanielAvdar@users.noreply.github.com>
2024-03-31 15:25:39 +0300

    Add package version to pandas-pyarrow module
    The code in pandas-pyarrow's `__init__.py` file was modified to import the `version` function from `importlib.metadata`. Used this function to set the `__version__` variable to the version of the pandas-pyarrow package. This change makes it easier to access and display the package version.
    
    Signed-off-by: DanielAvdar <66269169+DanielAvdar@users.noreply.github.com>

--- pandas_pyarrow/__init__.py
+++ pandas_pyarrow/__init__.py
@@ -1,4 +1,7 @@
+from importlib.metadata import version
+
 from .pda_converter import PandasArrowConverter
 
+__version__ = version("pandas-pyarrow")
 convert_to_pyarrow = PandasArrowConverter()
 __all__ = ["PandasArrowConverter", "convert_to_pyarrow"]
